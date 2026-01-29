1e386cfb9bc22a23e58cd1277db71807394f6575
RANGANATHAN21 <SRINIRANGA56@GMAIL.COM>
2026-01-29 21:46:05 +0800

    Level-2

--- src/main/java/Ranga.java
+++ src/main/java/Ranga.java
@@ -16,27 +16,46 @@ public class Ranga {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("____________________________________________________________");
+        // Storage for up to 100 items
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(" Welcome to Vought™ Interactive Systems");
         System.out.println(" I'm Ranga, your favourite VoughtBot! Definitely not a supe. Trust me.");
         System.out.println(logo);
-        System.out.println(" Type a command or type 'bye' if you want out NOW.");
+        System.out.println(" We record everything you say for quality assurance purposes. Type 'list' to see stored items, or 'bye' to GTFO.");
         System.out.println("____________________________________________________________");
 
+        label:
         while (true) {
             System.out.print("> ");
             String command = scanner.nextLine().trim();
-
-            // Case-sensitive exit
-            if (command.equals("bye")) {
-                System.out.println(" Clocking out. Try not to cause an international incident.");
-                break;
-            }
-            else if (command.isEmpty()) {
+            
+            switch (command) {
+            case "bye":
+                System.out.println(" Thank you for your commitment to keeping supes safe. Try not to cause an international incident!");
+                break label;
+            case "":
                 System.out.println(" Say something or I'll ping Homelander.");
-            }
-            else {
-                System.out.println(command);
+                break;
+            case "list":
+                if (taskCount == 0) {
+                    System.out.println(" Nothing stored. Even The Deep has more going on.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                break;
+            default:
+                if (taskCount < 100) {
+                    tasks[taskCount] = command;
+                    taskCount++;
+                    System.out.println(" added: " + command);
+                } else {
+                    System.out.println(" Memory full. This is why we can't have nice things.");
+                }
+                break;
             }
         }
 
