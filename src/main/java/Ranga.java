eca30f02074fb00bf87b00e0688b66e01a219540
RANGANATHAN21 <SRINIRANGA56@GMAIL.COM>
2026-01-29 21:26:33 +0800

    Level-1

--- src/main/java/Ranga.java
+++ src/main/java/Ranga.java
@@ -1,20 +1,46 @@
+import java.util.Scanner;
+
 public class Ranga {
+
     public static void main(String[] args) {
 
         String logo =
-                " ____                               \n"
-                        + "|  _ \\  __ _ _ __   __ _  __ _ \n"
-                        + "| |_) / _` | '_ \\ / _` |/ _` |\n"
-                        + "|  _ < (_| | | | | (_| | (_| |\n"
-                        + "|_| \\_\\__,_|_| |_|\\__, |\\__,_|\n"
-                        + "                   |___/        \n";
+                """
+                         ____
+                        |  _ \\  __ _ _ __   __ _  __ _
+                        | |_) / _` | '_ \\ / _` |/ _` |
+                        |  _ < (_| | | | | (_| | (_| |
+                        |_| \\_\\__,_|_| |_|\\__, |\\__,_|
+                                           |___/
+                        """;
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Ranga");
+        System.out.println(" Welcome to Vought™ Interactive Systems");
+        System.out.println(" I'm Ranga, your favourite VoughtBot! Definitely not a supe. Trust me.");
         System.out.println(logo);
-        System.out.println(" What can I do for you?");
+        System.out.println(" Type a command or type 'bye' if you want out NOW.");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        while (true) {
+            System.out.print("> ");
+            String command = scanner.nextLine().trim();
+
+            // Case-sensitive exit
+            if (command.equals("bye")) {
+                System.out.println(" Clocking out. Try not to cause an international incident.");
+                break;
+            }
+            else if (command.isEmpty()) {
+                System.out.println(" Say something or I'll ping Homelander.");
+            }
+            else {
+                System.out.println(command);
+            }
+        }
+
         System.out.println("____________________________________________________________");
+        scanner.close();
     }
 }
