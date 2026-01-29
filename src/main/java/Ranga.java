5e894915a1feca6c6047a9bd53f6a6e77de56478
RANGANATHAN21 <SRINIRANGA56@GMAIL.COM>
2026-01-29 16:48:00 +0800

    Level-0

---
+++ src/main/java/Ranga.java
@@ -0,0 +1,20 @@
+public class Ranga {
+    public static void main(String[] args) {
+
+        String logo =
+                " ____                               \n"
+                        + "|  _ \\  __ _ _ __   __ _  __ _ \n"
+                        + "| |_) / _` | '_ \\ / _` |/ _` |\n"
+                        + "|  _ < (_| | | | | (_| | (_| |\n"
+                        + "|_| \\_\\__,_|_| |_|\\__, |\\__,_|\n"
+                        + "                   |___/        \n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Ranga");
+        System.out.println(logo);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
